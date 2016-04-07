9017f5ee132c319f55caf2e3fc69c18aa7207ed7
Juan M. Uys <opyate@gmail.com>
2016-04-07 13:13:07 +0100

    link to uys.io

--- .run.sh
+++
@@ -1,6 +0,0 @@
-#jekyll serve -w -P 3030
-sudo docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:80:80 jekyll/jekyll
-echo
-echo "next:"
-echo "alias cp=cp"
-echo "cp -R _site/* ../opyate.github.io"
